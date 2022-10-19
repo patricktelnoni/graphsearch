bbdf75262ff8af48290681424b99bed518da0031
Patrick Telnoni <ptelnoni89@gmail.com>
2022-10-19 11:18:55 +0700

    Change Search Algorithm as Abstract class

--- com/graph/SearchAlgorithm.java
+++ com/graph/SearchAlgorithm.java
@@ -1,11 +1,9 @@
 package com.graph;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.Map;
 
-public class SearchAlgorithm {
+public abstract class SearchAlgorithm {
     protected HashMap<String, Double> pq = new HashMap<>();
     public ArrayList<String> visited = new ArrayList<>();
     public GraphHeuristic mapHeuristic;
